eb010e016a7f6735b916b893d0017902ac7cc0fd
Hugo Vega <hugo17avid@gmail.com>
2024-09-14 19:57:39 -0700

    Update cumlativeReturns.py

--- cumlativeReturns.py
+++ cumlativeReturns.py
@@ -3,21 +3,21 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf  # Import yfinance to fetch data
 
 def symbol_to_path(symbol, base_dir="data"):
     """Return CSV file path given ticker symbol."""
     return os.path.join(base_dir, "{}.csv".format(str(symbol)))
 
 def get_data(symbols, dates):
-    """Read stock data (adjusted close) for given symbols from CSV files."""
+    """Fetch stock data (adjusted close) for given symbols from Yahoo Finance."""
     df = pd.DataFrame(index=dates)
     if 'SPY' not in symbols:  # add SPY for reference, if absent
         symbols.insert(0, 'SPY')
 
     for symbol in symbols:
-        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
-                parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
-        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df_temp = yf.download(symbol, start=dates[0], end=dates[-1])['Adj Close']
+        df_temp = df_temp.rename(symbol)
         df = df.join(df_temp)
         if symbol == 'SPY':  # drop dates SPY did not trade
             df = df.dropna(subset=["SPY"])
